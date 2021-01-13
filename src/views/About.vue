240872c831edd8e7eb162c038f8cf5c7ea106269
Mathieu <math.oron@gmail.com>
2021-01-13 22:20:30 +0100

    try to add a component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,12 @@
-<template>
-  <h1>About Page</h1>
+<template lang="jade">
+  myComponenent
 </template>
+
+<script>
+  import myComponenent from './Header.vue'
+  export default {
+    components: {
+      myComponenent
+    }
+  }
+</script>
---
+++ src/views/Header.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1> *Send Help ? Hi ?</h1>
+  <h1> Is someone sees me ? :c </h1>
+</template>
