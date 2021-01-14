f7207c298c1d2f005305757ac4a5e301cc85601e
Mathieu <math.oron@gmail.com>
2021-01-14 11:00:40 +0100

    Adding header (with styles)

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,9 +1,40 @@
 <template>
-  <h2> My beautiful header !</h2>
-  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/help">Help</router-link>
-  </div>
-  <router-view />
+  <body>
+    <div class="topnav">
+      <router-link class="active" to="/">My calendar</router-link>
+      <router-link to="/">Calendars</router-link>
+      <router-link to="/">Profil</router-link>
+    </div>
+  </body>
 </template>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
