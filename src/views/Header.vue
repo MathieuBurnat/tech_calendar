11f0faee3fd7c12ffb73c9abdae6e77b271d26f7
Mathieu <math.oron@gmail.com>
2021-01-14 10:46:16 +0100

    Remove default content from App.Vue

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,3 +1,9 @@
 <template>
   <h2> My beautiful header !</h2>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/help">Help</router-link>
+  </div>
+  <router-view />
 </template>
