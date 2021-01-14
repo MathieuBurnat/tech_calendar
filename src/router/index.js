c6795176fd7e2a6c27ad2f2cbeea4ff5dab2b49f
Mathieu <math.oron@gmail.com>
2021-01-14 10:33:13 +0100

    Adding basic pages (they are actually empty)

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
-import Help from "@/views/Help.vue";
 import PageNotFound from "@/views/errors/PageNotFound.vue";
+import Home from "@/views/Home.vue";
+import PickACalender from "@/views/PickACalender.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
+import NewCalendar from "@/views/NewCalendar.vue";
+import Calendar from "@/views/Calendar.vue";
+import EditCase from "@/views/EditCase.vue";
+import EditType from "@/views/EditType.vue";
+import About from "@/views/About.vue";
+import Header from "@/views/Header.vue";
+import Footer from "@/views/Footer.vue";
+
 
 const routes = [
   {
@@ -16,9 +25,49 @@ const routes = [
     component: About,
   },
   {
-    path: "/help",
-    name: "Help",
-    component: Help,
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/newcalendar",
+    name: "NewCalendar",
+    component: NewCalendar,
+  },
+  {
+    path: "/pickacalendar",
+    name: "PickACalender",
+    component: PickACalender,
+  },
+  {
+    path: "/calendar",
+    name: "Calendar",
+    component: Calendar,
+  },
+  {
+    path: "/editcase",
+    name: "EditCase",
+    component: EditCase,
+  },
+  {
+    path: "/edittype",
+    name: "EditType",
+    component: EditType,
+  },
+  { //dev-options
+    path: "/dev-header",
+    name: "Header",
+    component: Header,
+  },
+  { //dev-options
+    path: "/dev-footer",
+    name: "Footer",
+    component: Footer,
   },
   {
     path: "/:catchAll(.*)",
