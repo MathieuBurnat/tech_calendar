a20b96a1fd593811f8feb99416e679c0bb27721a
Mathieu <math.oron@gmail.com>
2021-01-18 00:18:31 +0100

    Create test-data's page

---
+++ src/views/TestData.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header/>
+    <h2> Test. Test. Test-Data !</h2>
+</template>
+
+<script>
+  import Header from './Header.vue'
+
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
