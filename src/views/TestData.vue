8876a9c5495ae91d75204799b6e0649a60e86d1c
Mathieu <math.oron@gmail.com>
2021-01-18 00:32:50 +0100

    Add function test

--- src/views/TestData.vue
+++ src/views/TestData.vue
@@ -11,4 +11,17 @@
       Header
     }
   }
+
+
+  function connection(){
+    // getting-started.js
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+    // we're connected!
+    });
+  }
 </script>
