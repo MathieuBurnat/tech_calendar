0949f56ccb314c27579f61876b40bead3cec6eba
Mathieu <math.oron@gmail.com>
2021-01-18 22:52:51 +0100

    Move register code into the register's vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,54 @@
 <template>
   <Header/>
-    <h2> Welcome to the register's section. </h2>
+    <h1> S'enregistrer </h1>
+    <form @submit.prevent="login">
+            <label>Nom</label>
+            <input type="text" class="form-control" v-model="user.name" required>
+
+            <label>Adresse mail</label>
+            <input type="text" class="form-control" v-model="user.email" required>
+
+            <label>Mot de passe</label>
+            <input type="password" class="form-control" v-model="user.password" required>
+
+            <button class="btn btn-danger btn-block">S'enregistrer !</button>
+
+            ou <router-link class="active" to="/login">se connecter</router-link>.
+    </form>
   <Footer/>
 </template>
+
+<script>
+import Header from './Header.vue'
+import axios from "axios";
+
+export default {
+  components: {
+    Header
+  },
+  data() {
+    return {
+        user: {
+            name: '',
+            email: '',
+            password: ''
+        },
+    }
+  },
+  methods:{
+    login(){
+        let apiURL = 'http://localhost:4000/user/create-user';
+        axios.post(apiURL, this.user).then(() => {
+          //this.$router.push('/view')
+          this.user = {
+            name: '',
+            email: '',
+            password: ''
+          }
+        }).catch(error => {
+            console.log(error);
+        });
+      },
+  },
+}
+</script>
