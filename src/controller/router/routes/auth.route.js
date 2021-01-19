80f8d9be06c2a3ced396098678a61d2043f4197a
Mathieu <math.oron@gmail.com>
2021-01-19 13:30:16 +0100

    Create token schema with routes

---
+++ src/controller/router/routes/auth.route.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const authRoute = express.Router();
+const Bcrypt = require("bcryptjs");
+
+// auth model
+let authModel = require('../../../model/token');
+
+authRoute.route('/').get((req, res) => {
+    authModel.find((error, data) => {
+     if (error) {
+       return next(error)
+     } else {
+       res.json(data)
+     }
+   })
+ })
+
+ authRoute.route('/create-token').post((req, res, next) => {
+  authModel.create(req.body, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+});
+
+
+// Delete auth
+authRoute.route('/delete-auth/:id').delete((req, res, next) => {
+  authModel.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data
+      })
+    }
+  })
+})
+
+module.exports = authRoute;
