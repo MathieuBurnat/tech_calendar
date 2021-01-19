82705c976f565c97b904cb388e061dfb4b303269
Math <math.oron@gmail.com>
2021-01-20 00:59:50 +0100

    Able to create calendar if token is valid

--- src/views/NewCalendar.vue
+++ src/views/NewCalendar.vue
@@ -33,33 +33,34 @@ export default {
     createCalendar(){
       //I wanna try if we are able to verify the authenticity of the user
       var ui =  JSON.parse(localStorage.getItem("userInformations"));
-
+      var TockenValid = false;
 
       let authURL = 'http://localhost:4000/auth/verify-authenticity';
       axios.post(authURL, ui).then((res) => {
         
         console.log(res.data.message); //return message
-
-      }).catch(error => {
-          console.log(error);
-      });
-
-
-      // create the calendar with the text box name
-      let calendarURL = 'http://localhost:4000/calendar/create-calendar';
-      axios.post(calendarURL, this.calendar).then((res) => {
+        TockenValid = res.data.isTockenMatches;
         
-        console.log(res.data.message); //return message
+        if(TockenValid){
+          // create the calendar with the text box name
+          let calendarURL = 'http://localhost:4000/calendar/create-calendar';
+          axios.post(calendarURL, this.calendar).then((res) => {
+            
+            console.log(res.data.message); //return message
 
-        this.calendar = {
-          name: '',
+            this.calendar = {
+              name: '',
+            }
+          }).catch(error => {
+              console.log(error);
+          });
+        }else{ //the token is not valid... 
+          this.$router.push('/logout')
         }
       }).catch(error => {
           console.log(error);
       });
-
-
-      },
     },
+  },
 }
 </script>
