49f0924f290d63d4554e8e1f050b4b9ab8d56320
Mathieu <math.oron@gmail.com>
2021-01-20 17:21:13 +0100

    If there is no calendar. Chance the apsect of the view

--- src/views/PickACalender.vue
+++ src/views/PickACalender.vue
@@ -2,17 +2,29 @@
   <Header/>
   <h1> Pick a calendar ! </h1>
 
-  <label for="calendar-choice">calendars:</label>
-  <input type="text" v-model="calendar" list="calendars">
-  <datalist id="calendars">
-    <option v-for="calendar in calendars" :key="calendar.id">{{calendar.name}}</option>
-  </datalist>
+  <form @submit.prevent="goToCalendar">
 
-  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-  <label for="vehicle2"> Keep this calendar as default </label><br>
-  <input type="submit" value="Go">
-  <br/> ...ou créer un nouveau <router-link class="active" to="/newcalendar">calendrier</router-link>.
 
+    <div v-if="!calendars.length"> <!-- If there isn't calendars. -->
+      <p> Aucun calendrier n'existe pour le moment. </p>
+      Créer en un nouveau <router-link class="active" to="/newcalendar">ici</router-link> !
+    </div>
+    <div v-else> 
+      <label for="calendar-choice">calendars:</label>
+      <input type="text" v-model="calendar" list="calendars">
+      <datalist id="calendars">
+        <option v-for="calendar in calendars" :key="calendar.id">{{calendar.name}}</option>
+      </datalist>
+
+      <input type="checkbox" id="isChecked" name="isChecked" value="isChecked">
+      <label for="isChecked"> Keep this calendar as default </label><br>
+      <input type="submit" value="Go">
+      <br/> ...ou créer un nouveau <router-link class="active" to="/newcalendar">calendrier</router-link>.
+    </div>
+
+
+    
+  </form>
 </template>
 
 <script>
@@ -35,6 +47,11 @@
           console.log(error);
       });
     },
+    methods:{
+      goToCalendar(){
+        console.log("hello there");
+      }
+    }
   }
 
 </script>
