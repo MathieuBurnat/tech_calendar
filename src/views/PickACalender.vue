e5d0004f2a0a99e75dce8751996ca57dd04666aa
Mathieu <math.oron@gmail.com>
2021-01-20 16:45:25 +0100

    Resolve errors and update the view

--- src/views/PickACalender.vue
+++ src/views/PickACalender.vue
@@ -3,22 +3,10 @@
   <h1> Pick a calendar ! </h1>
 
   <label for="calendar-choice">calendars:</label>
-  <input list="calendar-list" id="calendar-choice" name="calendar-choice" />
-
-  <datalist id="calendar-list">
-  <option value="Chocolate"></option>
-  <option value="Coconut"></option>
-  <option value="Mint"></option>
-  <option value="Strawberry"></option>
-  <option value="Vanilla"></option>
+  <input type="text" v-model="calendar" list="calendars">
+  <datalist id="calendars">
+    <option v-for="calendar in calendars" :key="calendar.id">{{calendar.name}}</option>
   </datalist>
-
-  <h1> - Test - </h1>
-  <tr v-for="calendar in calendars" :key="calendar.id">
-    <td>{{calendar.name}}</td>
-    <td>{{calendar.author}}</td>
-  </tr>
-  <h1> - End - </h1>
 
   <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
   <label for="vehicle2"> Keep this calendar as default </label><br>
@@ -31,7 +19,6 @@
 
   import Header from './Header.vue'
   import axios from "axios";
-  import { ref } from 'vue';
 
   export default {
     components: {
@@ -43,17 +30,7 @@
     mounted(){
       let apiURL = 'http://localhost:4000/calendar/';
       axios.get(apiURL).then((res) => {
-      
-      console.log(JSON.stringify(res.data));
-
-
-      //this.calendars = ref([res.data]);
-      this.calendars = ref([
-        {"_id":"6008333a85179c0f042ef332","name":"My-Tech-calendar","__v":0},
-        {"_id":"600836e085179c0f042ef333","name":"chocoMelly","__v":0}
-      ]);
-
-      
+      this.calendars = res.data;
       }).catch(error => {
           console.log(error);
       });
