5de08205b12fa0df46738bf3d659a05cf4e93df3
Mathieu <math.oron@gmail.com>
2021-01-22 17:10:21 +0100

    [refactoring] #1 Adding one year to the layout

--- src/controller/router/routes/calendar.route.js
+++ src/controller/router/routes/calendar.route.js
@@ -136,7 +136,10 @@ calendarRoute.route('/').get((req, res) => {
         calendar : calendar_id
       }
 
-      createYear(newYear); //createYear -> creareTrimester -> createWeeks
+
+      for (let i = 0; i < 2; i++) { // TEST 
+        createYear(newYear); //createYear -> creareTrimester -> createWeeks
+      }
 
       response.send({ message: "The calendar is created !", isCreated, calendar_id });
     }
