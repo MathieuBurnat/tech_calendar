1acd7e104366420b29201258c334e501350b2b6a
Mathieu <math.oron@gmail.com>
2021-01-24 15:21:10 +0100

    I make it. We can now get the calendar ! *we are the champions's music*

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -27,28 +27,34 @@
       const route = useRoute()
       const id = computed(() => route.params.id);
       
-      var data = {
-        calendarId : id.value,
-      }
 
-      let apiURL = 'http://localhost:4000/calendar/get-full-calendar';
-      axios.post(apiURL, data).then((res) => {
-
-
-      console.log("Beep Bop Beep, I'm gonna crazy. Oh by the way you have a new message : " + res.data.message);
-      
-      //const message = "Don't trust the crazy wise";
-
-      }).catch(error => {
-          console.log(error);
-      });
       return {
         id,
       };
     },methods: {
       displayMySweetCalendar() {
-      return "poney " + this.id;
-  }
+
+      var data = {
+        calendarId : this.id,
+      }
+
+      const message = "Don't trust the crazy wise";
+
+
+      //get the full calendar
+      let apiURL = 'http://localhost:4000/calendar/get-full-calendar';
+      axios.post(apiURL, data).then((res) => {
+
+      console.log("Beep Bop Beep, I'm gonna crazy. Oh by the way you have a new message : " + res.data.message);
+
+      console.log(message);
+
+      }).catch(error => {
+          console.log(error);
+      });
+
+      return message;
+    }
 }
   }
 </script>
