d03865cc37b8b09bbcfb2f859dae1813dc564a60
Mathieu <math.oron@gmail.com>
2021-01-24 22:13:02 +0100

    Create function to read calendar's datas

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,15 +3,14 @@
     
     <div v-if="calendar.length == 0"> <!-- If there isn't calendars. -->
       <h2> Chargement du calendrier... </h2>
+      <p> {{ displayMySweetCalendar() }}</p>
     </div>
     <div v-else> 
       <h1>- [ * * * {{ calendar.name }} * * * ] - </h1>
-      <p> id : {{id}} </p>
 
-      <p> what's my name ? {{ calendar.name }}</p>
-      <p> {{ message }}</p>
+      <!-- Display-->
+      <div v-for="year in calendar.yearsList" :key="year.id">- trimesters - {{ year.id }} </div>
     </div>
-    <p> {{ displayMySweetCalendar() }}</p>
 
 </template>
 
@@ -29,7 +28,6 @@
     data() {
       return { 
         calendar: [], 
-        message : "",
         }
     },setup() {
       const route = useRoute()
@@ -44,25 +42,34 @@
           calendarId : this.id,
         }
 
-        var calendar = [];
-
         //get the full calendar
         let apiURL = 'http://localhost:4000/calendar/get-full-calendar';
         axios.post(apiURL, data).then((res) => {
 
         console.log("Beep Bop Beep, I'm gonna crazy. Oh by the way you have a new message : " + res.data.message);
 
-        calendar = res.data.holyCalendar;
-        console.log(calendar);
+        this.calendar = res.data.holyCalendar;
+        console.log(this.calendar);
 
 
         //tests section
-        this.message = "Hellow sweetie :3";
-        this.calendar = calendar;
+        console.log("[" + this.calendar.name + "]");
 
-        console.log("my name is " + calendar.name);
-        console.log("m-i " + !calendar.length);
+        for (let i = 0; i < this.calendar.yearsList.length; i++)  //inside the yearsList, get trismters
+        { 
+          console.log(i + "> my starting date : " + this.calendar.yearsList[i].startingDate);
 
+          for (let i2 = 0; i2 < this.calendar.yearsList[i].trimestersList.length; i2++)
+          {  
+            console.log(i+ "." + i2 + "> Trimester's name : " + this.calendar.yearsList[i].trimestersList[i2].name);
+
+            for (let i3 = 0; i3 < this.calendar.yearsList[i].trimestersList[i2].weeksList.length; i3++)
+            {  
+              console.log(i+ "." + i2 + "." + i3 + "> week's name : " + this.calendar.yearsList[i].trimestersList[i2].weeksList[i3].name);
+            }
+          }
+
+        }
 
         //
 
