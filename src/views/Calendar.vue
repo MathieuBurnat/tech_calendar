017377e606356181134bf5594fb0f75c8b0e114c
Mathieu <math.oron@gmail.com>
2021-01-24 19:38:33 +0100

    Prepare to get the calendar's name

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -4,8 +4,9 @@
     <h2> Welcome to the calendar's section. </h2>
     <p> id : {{id}} </p>
 
-    <p div="poney"> {{ displayMySweetCalendar() }}</p>
-    
+    <p> {{ displayMySweetCalendar() }}</p>
+    <p> what's my name ? {{ calendars.name }}</p>
+    <p> {{ message }}</p>
 </template>
 
 <script>
@@ -14,7 +15,8 @@
   import { computed } from "vue";
   import { useRoute } from "vue-router";
 
-  export default {
+  export default 
+    {
     components: {
       Header
     },
@@ -26,35 +28,35 @@
     },setup() {
       const route = useRoute()
       const id = computed(() => route.params.id);
-      
-
       return {
         id,
       };
-    },methods: {
+    },
+    methods: {
       displayMySweetCalendar() {
+        var data = {
+          calendarId : this.id,
+        }
 
-      var data = {
-        calendarId : this.id,
-      }
+        var calendar = [];
 
-      const message = "Don't trust the crazy wise";
+        //get the full calendar
+        let apiURL = 'http://localhost:4000/calendar/get-full-calendar';
+        axios.post(apiURL, data).then((res) => {
 
+        console.log("Beep Bop Beep, I'm gonna crazy. Oh by the way you have a new message : " + res.data.message);
 
-      //get the full calendar
-      let apiURL = 'http://localhost:4000/calendar/get-full-calendar';
-      axios.post(apiURL, data).then((res) => {
+        calendar = res.data.holyCalendar;
+        console.log(calendar);
 
-      console.log("Beep Bop Beep, I'm gonna crazy. Oh by the way you have a new message : " + res.data.message);
+        this.message = "poney";
 
-      console.log(message);
+        }).catch(error => {
+            console.log(error);
+        });
 
-      }).catch(error => {
-          console.log(error);
-      });
-
-      return message;
+        this.calendars = calendar;
+        }
     }
-}
   }
 </script>
