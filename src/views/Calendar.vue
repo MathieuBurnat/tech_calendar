dbe444de6953e7551f55eb375ad4730e70126cc2
Mathieu <math.oron@gmail.com>
2021-01-25 09:22:24 +0100

    [Refactoring] Update the graphical calendar, add <table> and new styles

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -24,40 +24,66 @@
 
 
     </div>
-
       <!-- the render ends here -->
       <!-- test (Here we'll render the calendar) -->
-      <div class="row">
-      <div class="column">
-        <div class="week">Some text</div>
-        <div class="week">Some text</div>
-        <div class="week">Some text</div>
-      </div>
-      <div class="column">
-        <div class="week">Some text</div>
-        <div class="week">Some text</div>        
-        <div class="week">Some text</div>        
-      </div>
-      </div>
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+      <tr>
+        <td>Ernst Handel</td>
+        <td>Roland Mendel</td>
+        <td>Austria</td>
+      </tr>
+      <tr>
+        <td>Island Trading</td>
+        <td>Helen Bennett</td>
+        <td>UK</td>
+      </tr>
+      <tr>
+        <td>Laughing Bacchus Winecellars</td>
+        <td>Yoshi Tannamuri</td>
+        <td>Canada</td>
+      </tr>
+      <tr>
+        <td>Magazzini Alimentari Riuniti</td>
+        <td>Giovanni Rovelli</td>
+        <td>Italy</td>
+      </tr>
+    </table>
       <!-- test -->
 
 </template>
 
 <style>
 
-.row {
-  display: flex;
-  flex-flow: row wrap;
-  width: 75%;
-  padding: 10px;
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  margin: 10px;
 }
 
-.column {
-  flex: 1 1 50%;
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
 }
 
-.week{
-  border: 1px solid #ccc;
+tr:nth-child(even) {
+  /*background-color: #dddddd;*/
 }
 
 </style>
