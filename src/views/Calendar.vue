18e6c7204ab5a6b8e02ea2c0174ede3f33287081
Mathieu <math.oron@gmail.com>
2021-01-25 09:42:48 +0100

    Update dirties

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -28,37 +28,30 @@
       <!-- test (Here we'll render the calendar) -->
     <table>
       <tr>
-        <th>Company</th>
         <th>Contact</th>
         <th>Country</th>
       </tr>
       <tr>
-        <td>Alfreds Futterkiste</td>
         <td>Maria Anders</td>
         <td>Germany</td>
       </tr>
       <tr>
-        <td>Centro comercial Moctezuma</td>
         <td>Francisco Chang</td>
         <td>Mexico</td>
       </tr>
       <tr>
-        <td>Ernst Handel</td>
         <td>Roland Mendel</td>
         <td>Austria</td>
       </tr>
       <tr>
-        <td>Island Trading</td>
         <td>Helen Bennett</td>
         <td>UK</td>
       </tr>
       <tr>
-        <td>Laughing Bacchus Winecellars</td>
         <td>Yoshi Tannamuri</td>
         <td>Canada</td>
       </tr>
       <tr>
-        <td>Magazzini Alimentari Riuniti</td>
         <td>Giovanni Rovelli</td>
         <td>Italy</td>
       </tr>
@@ -72,7 +65,7 @@
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
-  width: 100%;
+  width: 75%;
   margin: 10px;
 }
 
@@ -80,6 +73,7 @@ td, th {
   border: 1px solid #dddddd;
   text-align: left;
   padding: 8px;
+  width: 50% ;
 }
 
 tr:nth-child(even) {
